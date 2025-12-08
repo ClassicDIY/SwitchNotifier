c636cec2c5ff11d04e34238e9e0e274d34b158d2
Graham Ross <graham.a.ross@gmail.com>
2025-12-08 12:42:40 -0500

    Update AP_TIMEOUT and reorder port defines
    Set AP_TIMEOUT to 30000 and updated comment to indicate production value. Moved ASYNC_WEBSERVER_PORT and DNS_PORT defines for better organization.

--- code/ESP32/main/include/Defines.h
+++ code/ESP32/main/include/Defines.h
@@ -14,16 +14,15 @@
 #define AP_BLINK_RATE 600
 #define NC_BLINK_RATE 100
 
-// #define AP_TIMEOUT 30000 //set back to 1000 in production
-// #define AP_TIMEOUT 30000
-#define AP_TIMEOUT 1000
+// #define AP_TIMEOUT 1000 //set back to 30000 in production
+#define AP_TIMEOUT 30000 
 #define FLASHER_TIMEOUT 10000
 #define GPIO0_FactoryResetCountdown 5000 // do a factory reset if GPIO0 is pressed for GPIO0_FactoryResetCountdown
 #define WS_CLIENT_CLEANUP 5000
 #define WIFI_CONNECTION_TIMEOUT 120000
 #define DEFAULT_AP_PASSWORD "12345678"
-
-#define WATCHDOG_TIMER 600000 // time in ms to trigger the watchdog
+#define ASYNC_WEBSERVER_PORT 80
+#define DNS_PORT 53
 
 #define SMTP_server "smtp.gmail.com"
 #define SMTP_Port 465
@@ -31,5 +30,4 @@
 #define NTP_Server1 "pool.ntp.org"
 #define NTP_Server2 "time.nist.gov"
 
-#define ASYNC_WEBSERVER_PORT 80
-#define DNS_PORT 53
+
